2014af0ca2ba22ee0850e50ff51d3575ef56a2d9
Abhik Banerjee <abhik.banerjee@thewitslab.com>
2024-04-28 12:52:44 +0530

    Added fix to provider name placement.

--- src/components/QrDialog.tsx
+++ src/components/QrDialog.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Dialog from "@mui/material/Dialog";
-import { pdf, transformJSON } from "../utils";
+import { getScale, pdf, transformJSON } from "../utils";
 import { QRCode } from "react-qrcode-logo";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
@@ -11,175 +11,151 @@ import { useTheme } from "@mui/material/styles";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 type QrDialogProps = {
-  onClose: () => void;
-  open: boolean;
-  qrData: string;
-  providerName: string;
+	onClose: () => void;
+	open: boolean;
+	qrData: string;
+	providerName: string;
 };
 
 export const QrDialog = ({
-  onClose,
-  open,
-  qrData,
-  providerName,
+	onClose,
+	open,
+	qrData,
+	providerName,
 }: QrDialogProps) => {
-  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
-  const [logoProps, setLogoProps] = useState({
-    logoImage: "",
-    logoHeight: 0,
-    logoWidth: 0,
-  });
-  // const [width, setWidth] = useState(window.innerWidth);
+	const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+	const [logoProps, setLogoProps] = useState({
+		logoImage: "",
+		logoHeight: 0,
+		logoWidth: 0,
+	});
+	// const [width, setWidth] = useState(window.innerWidth);
 
-  // useEffect(() => {
-  //   setWidth(window.innerWidth);
-  // }, []);
+	// useEffect(() => {
+	//   setWidth(window.innerWidth);
+	// }, []);
 
-  const theme = useTheme();
-  const viewportWidth = window.innerWidth;
+	const theme = useTheme();
+	const viewportWidth = window.innerWidth;
 
-  const breakpoints = {
-    xs: theme.breakpoints.values.xs, // e.g., 0
-    sm: theme.breakpoints.values.sm, // e.g., 600
-    md: theme.breakpoints.values.md, // e.g., 960
-    lg: theme.breakpoints.values.lg, // e.g., 1280
-    xl: theme.breakpoints.values.xl, // e.g., 1536
-  };
+	const breakpoints = {
+		xs: theme.breakpoints.values.xs, // e.g., 0
+		sm: theme.breakpoints.values.sm, // e.g., 600
+		md: theme.breakpoints.values.md, // e.g., 960
+		lg: theme.breakpoints.values.lg, // e.g., 1280
+		xl: theme.breakpoints.values.xl, // e.g., 1536
+	};
 
-  let qrScale = 0.165;
+	const qrScale = useMemo(
+		() => getScale(viewportWidth, breakpoints),
+		[viewportWidth]
+	);
 
-  if (viewportWidth < 420) {
-    qrScale = 0.094;
-  } else if (viewportWidth >= 420 && viewportWidth < breakpoints.sm) {
-    qrScale = 0.11;
-  } else if (
-    viewportWidth >= breakpoints.sm &&
-    viewportWidth < breakpoints.md
-  ) {
-    qrScale = 0.165;
-  } else if (
-    viewportWidth >= breakpoints.md &&
-    viewportWidth < breakpoints.lg
-  ) {
-    qrScale = 0.165;
-  } else if (
-    viewportWidth >= breakpoints.lg &&
-    viewportWidth < breakpoints.xl
-  ) {
-    qrScale = 0.165;
-  } else {
-    qrScale = 0.165;
-  }
+	useEffect(() => {
+		const generatePdf = async () => {
+			setTimeout(async () => {
+				const canvas: any = document.getElementById("qr-code-component");
 
-  useEffect(() => {
-    const generatePdf = async () => {
-      setTimeout(async () => {
-        const canvas: any = document.getElementById("qr-code-component");
+				if (canvas) {
+					const pngUrl = canvas
+						.toDataURL("image/png")
+						.replace("image/png", "image/octet-stream");
 
-        if (canvas) {
-          const pngUrl = canvas
-            .toDataURL("image/png")
-            .replace("image/png", "image/octet-stream");
+					const pdfCreated = await pdf(pngUrl, providerName, qrScale);
+					const blob = new Blob([pdfCreated as Uint8Array], {
+						type: "application/pdf",
+					});
+					const blobUrl = URL.createObjectURL(blob);
+					// console.log("blobUrl", blobUrl);
+					setPdfUrl(blobUrl);
+					// console.log("blobUrl", blobUrl);
+				}
+			}, 10);
+		};
 
-          const pdfCreated = await pdf(pngUrl, providerName, qrScale);
-          const blob = new Blob([pdfCreated as Uint8Array], {
-            type: "application/pdf",
-          });
-          const blobUrl = URL.createObjectURL(blob);
-          console.log("blobUrl", blobUrl);
-          setPdfUrl(blobUrl);
-          console.log("blobUrl", blobUrl);
-        }
-      }, 10);
-    };
+		if (open) {
+			generatePdf();
+		}
+	}, [open]);
 
-    if (open) {
-      generatePdf();
-    }
-  }, [open]);
+	useEffect(() => {
+		const logo = new Image();
+		logo.src = "./ondc-network-vertical.png";
 
-  useEffect(() => {
-    const logo = new Image();
-    logo.src = "./ondc-network-vertical.png";
+		logo.onload = () => {
+			const basewidth = 100;
+			const wpercent = basewidth / logo.width;
+			const hsize = logo.height * wpercent;
+			const canvas = document.createElement("canvas");
+			canvas.width = basewidth;
+			canvas.height = hsize;
+			const ctx = canvas.getContext("2d");
 
-    logo.onload = () => {
-      const basewidth = 100;
-      const wpercent = basewidth / logo.width;
-      const hsize = logo.height * wpercent;
-      const canvas = document.createElement("canvas");
-      canvas.width = basewidth;
-      canvas.height = hsize;
-      const ctx = canvas.getContext("2d");
+			if (ctx) {
+				ctx.drawImage(logo, 0, 0, basewidth, hsize);
+				const resizedLogoDataURL = canvas.toDataURL();
 
-      if (ctx) {
-        ctx.drawImage(logo, 0, 0, basewidth, hsize);
-        const resizedLogoDataURL = canvas.toDataURL();
+				setLogoProps({
+					logoImage: resizedLogoDataURL,
+					logoHeight: hsize,
+					logoWidth: basewidth,
+				});
+			}
+		};
+	}, []);
 
-        setLogoProps({
-          logoImage: resizedLogoDataURL,
-          logoHeight: hsize,
-          logoWidth: basewidth,
-        });
-      }
-    };
-  }, []);
+	const downloadQr = async () => {
 
-  const downloadQr = async () => {
-    console.log("Downloading QR");
+		const canvas: any = document.getElementById("qr-code-component");
+		if (canvas) {
+			const pngUrl = canvas
+				.toDataURL("image/png")
+				.replace("image/png", "image/octet-stream");
 
-    const canvas: any = document.getElementById("qr-code-component");
-    console.log(canvas);
-    if (canvas) {
-      const pngUrl = canvas
-        .toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
+			const pdfCreated = await pdf(pngUrl, providerName, qrScale);
+			const blob = new Blob([pdfCreated as Uint8Array], {
+				type: "application/pdf",
+			});
+			const blobUrl = URL.createObjectURL(blob);
 
-      const pdfCreated = await pdf(pngUrl, providerName, qrScale);
-      const blob = new Blob([pdfCreated as Uint8Array], {
-        type: "application/pdf",
-      });
-      const blobUrl = URL.createObjectURL(blob);
-      console.log("PDF GENERATED", pdfCreated);
+			let downloadLink = document.createElement("a");
+			downloadLink.href = blobUrl; //pngUrl
+			downloadLink.download = `${providerName} QR.pdf`;
+			document.body.appendChild(downloadLink);
+			downloadLink.click();
+			document.body.removeChild(downloadLink);
+		}
+	};
 
-      let downloadLink = document.createElement("a");
-      downloadLink.href = blobUrl; //pngUrl
-      downloadLink.download = `${providerName} QR.pdf`;
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
-    }
-  };
+	const link = transformJSON(qrData);
 
-  const link = transformJSON(qrData);
 
-  console.log("pdfUrl", pdfUrl);
-
-  return (
-    <Dialog onClose={onClose} open={open} maxWidth="md">
-      <DialogContent>
-        {pdfUrl && (
-          <Document file={pdfUrl}>
-            <Page pageNumber={1} />
-          </Document>
-        )}
-        <QRCode
-          size={450}
-          ecLevel="H"
-          quietZone={4}
-          value={link as string}
-          logoImage={logoProps.logoImage}
-          logoHeight={logoProps.logoHeight}
-          logoWidth={logoProps.logoWidth}
-          id="qr-code-component"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button variant="contained" onClick={downloadQr}>
-          Download QR
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog onClose={onClose} open={open} maxWidth="md">
+			<DialogContent>
+				{pdfUrl && (
+					<Document file={pdfUrl}>
+						<Page pageNumber={1} />
+					</Document>
+				)}
+				<QRCode
+					size={450}
+					ecLevel="H"
+					quietZone={4}
+					value={link as string}
+					logoImage={logoProps.logoImage}
+					logoHeight={logoProps.logoHeight}
+					logoWidth={logoProps.logoWidth}
+					id="qr-code-component"
+				/>
+			</DialogContent>
+			<DialogActions>
+				<Button variant="contained" onClick={downloadQr}>
+					Download QR
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
 };
 
 export default QrDialog;
--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -1,74 +1,79 @@
 import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+import { getTextPlacement } from "./getTextPlacement";
 
 export async function pdf(
-  qrCodeImageBytes: any,
-  providerName: string,
-  scale: number
+	qrCodeImageBytes: any,
+	providerName: string,
+	scale: number
 ) {
-  try {
-    // Load existing PDF
-    const response = await fetch("Poster.pdf");
-    const existingPdfBytes = await response.arrayBuffer();
+	try {
+		// Load existing PDF
+		const response = await fetch("Poster.pdf");
+		const existingPdfBytes = await response.arrayBuffer();
 
-    // Load QR code image
-    // const qrCodeImageBytes = await fs.readFile('qr.png'); // Assuming the QR code image is in the same directory
-    // console.log("qrCodeImageBytes", qrCodeImageBytes)
-    // const qrCodeImage = await PDFDocument.createEmbeddedPng(qrCodeImageBytes);
-    // console.log("qrCodeImage", qrCodeImage)
+		// Load QR code image
+		// const qrCodeImageBytes = await fs.readFile('qr.png'); // Assuming the QR code image is in the same directory
+		// console.log("qrCodeImageBytes", qrCodeImageBytes)
+		// const qrCodeImage = await PDFDocument.createEmbeddedPng(qrCodeImageBytes);
+		// console.log("qrCodeImage", qrCodeImage)
 
-    // Load PDF document
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+		// Load PDF document
+		const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
-    const qrImage = await pdfDoc.embedPng(qrCodeImageBytes);
-    // console.log("qrImage", qrImage)
+		const qrImage = await pdfDoc.embedPng(qrCodeImageBytes);
+		// console.log("qrImage", qrImage)
 
-    // Embed QR code image
+		// Embed QR code image
 
-    const qrDims = qrImage.scale(scale); // Adjust size as needed
-    // console.log("qrDims", qrDims)
-    // console.log("qrImage", qrImage)
-    const page = pdfDoc.getPages()[0]; // Assuming first page
-    const qrX = 230; // X coordinate
-    const qrY = 500; // Y coordinate
-    page.drawImage(qrImage, {
-      x: qrX,
-      y: qrY,
-      width: qrDims.width,
-      height: qrDims.height,
-    });
+		const qrDims = qrImage.scale(scale); // Adjust size as needed
+		// console.log("qrDims", qrDims)
+		// console.log("qrImage", qrImage)
+		const page = pdfDoc.getPages()[0]; // Assuming first page
+		const qrX = 230; // X coordinate
+		const qrY = 500; // Y coordinate
+		page.drawImage(qrImage, {
+			x: qrX,
+			y: qrY,
+			width: qrDims.width,
+			height: qrDims.height,
+		});
 
-    page.setFontSize(35);
+		const helveticaFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
-    const helveticaFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+		providerName = providerName.replace(/(\w)(\w*)/g, function (_g0, g1, g2) {
+			return g1.toUpperCase() + g2.toLowerCase();
+		});
 
-    const s = providerName.replace(/(\w)(\w*)/g, function (g0, g1, g2) {
-      console.log(g0, g1, g2)
-      return g1.toUpperCase() + g2.toLowerCase();
-    });
+		const pageWidth = page.getWidth();
 
-    page.drawText(
-      s.length >= 20
-        ? `${s.substring(0, 20).trim()}...`
-        : s,
-      {
-        x: 190,
-        y: 700,
-        size: 26,
-        font: helveticaFont,
-        color: rgb(0.35, 0.35, 0.35),
-        
-      }
-    );
+		const textWithPositions = getTextPlacement(
+			providerName,
+			700,
+			pageWidth,
+			helveticaFont
+		);
+		textWithPositions.forEach(({ text, x, y, size, lineHeight }) => {
+			page.drawText(text, {
+				x,
+				y,
+				size,
+				font: helveticaFont,
+				color: rgb(0.35, 0.35, 0.35),
+				lineHeight,
+				// textAlign: TextAlignment.Center,
+			});
+		});
 
-    // Save modified PDF
-    const modifiedPdfBytes = await pdfDoc.save();
-    // fs.writeFileSync('../assets/modified.pdf', modifiedPdfBytes);
 
-    console.log("PDF modification completed successfully.");
-    return modifiedPdfBytes;
-  } catch (error) {
-    console.error("Error:", error);
-  }
+		// Save modified PDF
+		const modifiedPdfBytes = await pdfDoc.save();
+		// fs.writeFileSync('../assets/modified.pdf', modifiedPdfBytes);
+
+		console.log("PDF modification completed successfully.");
+		return modifiedPdfBytes;
+	} catch (error) {
+		console.error("Error:", error);
+	}
 }
 
 // pdf();
